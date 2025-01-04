8794ee170d6fc083a9311361f8bfaefe8118b2ae
Kunal Verma <127933656+Kunalverma45@users.noreply.github.com>
2025-01-04 13:24:40 +0530

    Update Signin.jsx

--- client/src/components/Auth/Signin.jsx
+++ client/src/components/Auth/Signin.jsx
@@ -24,7 +24,7 @@ const Signin = ({ isOpen, setIsOpen }) => {
         e.preventDefault();
         try {
             // Fetch all user data from the backend
-            const { data } = await axios.get("https://zomatocom-api.vercel.app/");
+            const { data } = await axios.get("https://zomatocom-api.vercel.app/users");
 
             // Check if email exists in the data
             const user = data.find((u) => u.email === userData.email);
